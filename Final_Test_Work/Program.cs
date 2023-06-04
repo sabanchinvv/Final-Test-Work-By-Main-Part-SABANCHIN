2141b4117b6f7ce9d90e52473459f1b4f5840a35
sabanchinvv <sabanchinvv@gmail.com>
2023-06-04 12:45:08 +0300

    Программа через цикл foreach

--- Final_Test_Work/Program.cs
+++ Final_Test_Work/Program.cs
@@ -10,36 +10,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Задаем исходный массив строк
-        string[] strings = {"Hello GeekBrains", "I Wanna McDonalds", "Ifh", "I Hate Instasamka", "Rose Is The Most Beautiful Flower"};
+        string[] strings = { "abc", "defg", "hi", "jklmn" };
+        string[] filteredStrings = new string[strings.Length];
+        int index = 0;
 
-        // Создаем новый массив куда будем записывать строки длиной не более 3 символов
-        string[] shortStrings = new string[strings.Length];
-        int count = 0;
-
-        // Проходим по всем элементам исходного массива
-        for (int i = 0; i < strings.Length; i++)
+        foreach (string s in strings)
         {
-            // Если длина строки меньше или равна 3 символам,
-            // то добавляем ее в новый массив
-            if (strings[i].Length <= 3)
+            if (s.Length <= 3)
             {
-                shortStrings[count] = strings[i];
-                count++;
+                filteredStrings[index] = s;
+                index++;
             }
         }
 
-        // Создаем новый массив нужной длины из корректных строк
-        string[] resultStrings = new string[count];
-        Array.Copy(shortStrings, resultStrings, count);
+        Array.Resize(ref filteredStrings, index);
 
-        // Выводим получившийся массив
-        Console.WriteLine("Массив строк длиной не более 3 символов: ");
-        foreach (string str in resultStrings)
+        Console.WriteLine("Original Strings:");
+        foreach (string s in strings)
         {
-            Console.WriteLine(str);
+            Console.WriteLine(s);
         }
 
-        Console.ReadKey();
+        Console.WriteLine("Filtered Strings:");
+        foreach (string s in filteredStrings)
+        {
+            Console.WriteLine(s);
+        }
     }
 }
